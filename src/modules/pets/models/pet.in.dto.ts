3b914d888c8b67b84e80c5035f9bd344cdc82911
Didier <didorg@gmail.com>
2021-09-15 16:40:24 -0400

    Add: class-validator

--- src/modules/pets/models/pet.in.dto.ts
+++ src/modules/pets/models/pet.in.dto.ts
@@ -1,11 +1,16 @@
-import { Field, InputType, Int } from "@nestjs/graphql";
+import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsAlpha, IsNotEmpty } from 'class-validator';
 
 @InputType()
 export class PetInputDTO {
   @Field(() => Int, { nullable: false })
   id: number;
+
   @Field()
+  @IsNotEmpty()
+  @IsAlpha()
   name: string;
+
   @Field({ nullable: true })
   type?: string;
 }
