b17af1cee923bad0b3a5478792b4adc18c13a47d
Didier <dginebra@altimetrik.com>
2021-10-14 13:23:28 -0400

    working on Owner Service

--- src/modules/owners/dto/owner.in.dto.ts
+++ src/modules/owners/dto/owner.in.dto.ts
@@ -5,7 +5,7 @@ import { PetInputDTO } from 'src/modules/pets/dto/pet.in.dto';
 
 @InputType()
 export class OwnerInputDTO {
-  @Field(() => Int, { description: 'description (placeholder)' })
+  @Field(() => Int, { nullable: true,  description: 'description (placeholder)'})
   id: number;
 
   @Field()
--- src/modules/owners/services/owners.service.ts
+++ src/modules/owners/services/owners.service.ts
@@ -13,29 +13,65 @@ export class OwnersService {
     @InjectRepository(Owner)
     private ownerRepository: Repository<Owner>){}
 
-  async createOwner(ownerInputDTO: OwnerInputDTO): Promise<OwnerOutputDTO>{
-     // We can also use a mapper
-     const owner = new Owner();
-     owner.id = ownerInputDTO.id;
-     owner.name = ownerInputDTO.name;
-     owner.pets = ownerInputDTO.pets;
- 
-     const ownerCreated = this.ownerRepository.create(owner);
-     const ownerSaved = await this.ownerRepository.save(ownerCreated);
-     const ownerOutputDTO = await this.mapperOwnerToOwnerOutputDTO(ownerSaved);
-     return ownerOutputDTO;
-  }
-  private async mapperOwnerToOwnerOutputDTO(ownerSaved: Owner):Promise<OwnerOutputDTO> {
-    throw new Error('Method not implemented.');
-  }
+    async createOwner(ownerIn: OwnerInputDTO): Promise<OwnerOutputDTO> {
+      const owner: Owner = await this.mapperOwnerInputDTOToOwner(ownerIn);
+      const ownerCreated: Owner = this.ownerRepository.create(owner);
+      const ownerSaved: Owner = await this.ownerRepository.save(ownerCreated);
+      const ownerOut: OwnerOutputDTO = await this.mapperOwnerToOwnerOutputDTO(ownerSaved);
+      return ownerOut;
+    }
+  
+    async findAll(): Promise<OwnerOutputDTO[]> {
+      const owners = await this.ownerRepository.find();
+      const ownerOutputDTOs = await this.mapperOwnersToOwnerOutputDTOs(owners);
+      return ownerOutputDTOs;
+    }
+  
+    async findOne(id: number): Promise<OwnerOutputDTO> {
+      const ownerById: Owner = await this.ownerRepository.findOneOrFail(id);
+      const ownerOutputDTO = await this.mapperOwnerToOwnerOutputDTO(ownerById);
+      return ownerOutputDTO;
+    }
+  
+    //Mappers
+    // TODO: (To achieve single responsibility) Move these Mappers to a Class OwnerServiceMapper and Inject it here.
+    private async mapperOwnerInputDTOToOwner(ownerInputDTO: OwnerInputDTO): Promise<Owner> {
+      try {
+        const owner = new Owner();
+        owner.id = ownerInputDTO.id;
+        owner.name = ownerInputDTO.name;
+        owner.pets = ownerInputDTO.pets;
+        return owner;
+      } catch (e) {
+        throw e;
+      }
+    }
 
-  // findAll() {
-  //   return `This action returns all owners`;
-  // }
+    private async mapperOwnersToOwnerOutputDTOs(
+      owners: Owner[],
+    ): Promise<OwnerOutputDTO[]> {
+      let ownersOut: OwnerOutputDTO[] = [];
+  
+      for (let owner of owners) {
+        let ownerOut = await this.mapperOwnerToOwnerOutputDTO(owner);
+        ownersOut.push(ownerOut);
+      }
+  
+      return ownersOut;
+    }
+  
+    private async mapperOwnerToOwnerOutputDTO(owner: Owner): Promise<OwnerOutputDTO> {
+      try {
+        const ownerOut = new OwnerOutputDTO();
+        ownerOut.id = owner.id;
+        ownerOut.name = owner.name;
+        ownerOut.pets = owner.pets;
 
-  // findOne(id: number) {
-  //   return `This action returns a #${id} owner`;
-  // }
+        return ownerOut;
+      } catch (e) {
+        throw e;
+      }
+    }
 
   // update(id: number, updateOwnerInput: OwnerOutputDTO) {
   //   return `This action updates a #${id} owner`;
